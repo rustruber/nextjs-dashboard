88b0cbfb93574ef6f6eec108d287a2eb47f9a323
SETTER <lphp@mail.ru>
2026-04-11 11:31:28 +0300

    feat: create strimming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,14 +7,14 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 export async function fetchRevenue() {
     try {
         // Искусственно задержка ответа в демонстрационных целях.
-        // Не делайте этого в продакшене :)
+        // Не делайте этого в production:)
 
-        // console.log('Fetching revenue data...');
-        // await new Promise((resolve) => setTimeout(resolve, 3000));
+        console.log('Fetching revenue data...');
+        await new Promise((resolve) => setTimeout(resolve, 3000));
 
         const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-        // console.log('Data fetch completed after 3 seconds.');
+        console.log('Data fetch completed after 3 seconds.');
 
         return data;
     } catch (error) {
